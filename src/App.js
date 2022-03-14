e82ce07bfc42aa2c08b08f923dee1026f3b75cae
Andreas Benckert <andreas.benckert@gmail.com>
2022-03-14 15:05:37 +0100

    Sidebar Klar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 
+import Navbar from "./componenets/Navbar";
+import Page1 from "./componenets/Page1";
+import Page2 from "./componenets/Page2";
+import Page3 from "./componenets/Page3";
+import Page4 from "./componenets/Page4";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
