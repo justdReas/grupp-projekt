9595034192bfbca698b04b470f53a738ed83d8f4
Kim Michael Garcia Haas <Kgh13690@gmail.com>
2022-03-15 12:10:10 +0100

    SubMenuLink

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Page1 from "./componenets/Page1";
 import Page2 from "./componenets/Page2";
 import Page3 from "./componenets/Page3";
 import Page4 from "./componenets/Page4";
+import Fullstackdeveloper from "./componenets/Utbildningar/Fullstackdeveloper";
+
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
-        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page3" element={<Page3 />} >
+         < Route path="Fullstackdeveloper" element={<Fullstackdeveloper />} />
+        </Route>
         <Route path="/page4" element={<Page4 />} />
       </Routes>
     </div>
