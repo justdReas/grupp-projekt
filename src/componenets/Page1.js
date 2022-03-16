38798ae52d7f6f8956df62fe0dc85f3c7d67cc80
Andreas Benckert <andreas.benckert@gmail.com>
2022-03-16 15:01:28 +0100

    Open/Close

--- src/componenets/Page1.js
+++ src/componenets/Page1.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./page1.css";
 
-function Page1() {
+const Page1 = () => {
+  const [create, setCreate] = useState(false);
+  const handleOpen = (event) => {
+    event.preventDefault();
+    setCreate(true);
+  };
+
   return (
     <div>
-      <h1 style={{ color: "pink" }}>Page 1</h1>
-      <button id="createBtn">Create a new account</button>
-      <form id="createForm">
-        <label>Please fill in details bellow:</label>
-        <input type="text" id="fname" placeholder="First name..." />
-        <input type="text" id="sname" placeholder="Surname..." />
-        <input type="text" id="email" placeholder="E-mail..." />
-        <input type="text" id="bank" placeholder="Bank account number..." />
-        <button id="cancel">Cancel</button>
-        <button id="create">Create</button>
-      </form>
+      <h1 style={{ color: "pink" }}>Staff</h1>
+      <button onClick={handleOpen} id="createBtn">
+        Create a new account
+      </button>
+      {create && (
+        <form id="createForm">
+          <label>Please fill in details bellow:</label>
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            placeholder="First name..."
+          />
+          <input type="text" id="sname" name="sname" placeholder="Surname..." />
+          <input type="email" id="email" name="email" placeholder="E-mail..." />
+          <input
+            type="text"
+            id="bank"
+            name="bank"
+            placeholder="Bank account number..."
+          />
+          <button id="cancel">Cancel</button>
+          <button id="create">Create</button>
+        </form>
+      )}
     </div>
   );
-}
+};
 
 export default Page1;
